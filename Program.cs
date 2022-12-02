fc3be24ea7b00d41f72364c031aaa0150104f502
Sedjex <kathshem@gmail.com>
2022-12-02 11:43:06 +0200

    Comment

--- Program.cs
+++ Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Visual Studio 2022 on Mac is unstable. I rolled back to 2019 but with .net sdk 7.0");
